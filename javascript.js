2395778379badb10aa295df36504ba4b2b9bf96c
Rez38 <136545384+Rez-38@users.noreply.github.com>
2025-10-02 20:05:46 -0700

    Add clear button & Size display
    Reset grid at the click of a button

--- javascript.js
+++ javascript.js
@@ -1,5 +1,12 @@
 console.log("Hello World!");
 
+// Nerdy/core logic setup...
+let size = 16;
+let totalTiles = size * size;
+const boardSize = 960;
+let tileSize = boardSize / size;
+
+
 // Prompt button...
 const sizeBtn = document.querySelector('#sizeBtn');
 sizeBtn.addEventListener('click', () => {
@@ -7,7 +14,7 @@ sizeBtn.addEventListener('click', () => {
     newSize = prompt('Select grid size (default is 16)...', '1-100');
     console.log(newSize);
     newSize = Number(newSize);
-
+    
     if (!newSize) {
         return;
     } else if (size === newSize) {
@@ -15,63 +22,50 @@ sizeBtn.addEventListener('click', () => {
     } else if (newSize < 1 || newSize > 100) {
         return;
     }
-
+    
     size = newSize;
     totalTiles = size * size;
     tileSize = boardSize / size;
-
+    
     clearGrid();
     console.log("grid has been cleared");
     generateGrid();
     tileBehavior();
-
-    // const currentSize = document.createElement('p');
-    // currentSize.classList.toggle('currentSize');
-    // currentSize.textContent = `${size} x ${size}`;
-    // const buttons = document.querySelector('.buttons');
-    // buttons.appendChild(currentSize);
+    
+    currentSize.textContent = `${size} x ${size}`;
 });
 
+// Grid Size Display... DOM Node practice
+const currentSize = document.createElement('p');
+    currentSize.classList.toggle('currentSize');
+    currentSize.textContent = `${size} x ${size}`;
+    currentSize.style.fontFamily = 'Ariel, sans-serif';
+    currentSize.style.fontSize = '22px';
+    currentSize.style.marginTop = '0px';
+    currentSize.style.marginBottom = '0px';
+    // currentSize.style.textAlign = 'center';
+
+    sizeBtn.appendChild(currentSize);
+
+// Board setup...
 const board = document.querySelector('#board');
-const boardSize = 960;
-board.style.width = `${boardSize}px`;
-board.style.height = `${boardSize}px`;
-board.style.backgroundColor = 'white';
+    board.style.width = `${boardSize}px`;
+    board.style.height = `${boardSize}px`;
+    board.style.backgroundColor = 'white';
 
-
-let size = 16;
-let totalTiles = size * size;
-
-let tileSize = boardSize / size;
-
+// Create Divs...
 generateGrid();
-// for (let i = 0; i < totalTiles; i++) {
-//     const tile = document.createElement('div');
-//     tile.classList.toggle('tile');
-
-//     tile.style.width = `${tileSize}px`;
-//     tile.style.height = `${tileSize}px`;
-
-//     board.appendChild(tile);
-// };
-
-// Tile behavior...
+// Change div/tile color...
 tileBehavior();
-// const tiles = document.querySelectorAll('.tile');
-// tiles.forEach((tile) => {
-//     tile.addEventListener('mouseover', () => {
-//         tile.style.backgroundColor = 'navy';
-//         console.log("navy");
-//     });
-// });
 
-// const currentSize = document.createElement('p');
-// currentSize.classList.toggle('currentSize');
-// currentSize.textContent = `${size} x ${size}`;
-// const buttons = document.querySelector('.buttons');
-// buttons.appendChild(currentSize);
-
-
+// Clear Button...
+const clearBtn = document.querySelector('#clearBtn');
+clearBtn.addEventListener('click', () => {
+    clearGrid();
+    console.log('grid has been cleared');
+    generateGrid();
+    tileBehavior();
+});
 // ================
 // Function Bank  =
 // ================
@@ -103,8 +97,4 @@ function tileBehavior() {
             console.log("navy");
         });
     });
-};
-
-function generateBoard() {
-
 };
